b6f44f45770e9ab88b895ac0d31805fc1267f3fe
Harshit Singh <120363478+harshitvj021@users.noreply.github.com>
2024-03-10 19:50:04 +0530

    Add files via upload

---
+++ HPC_Module 1_Day 10_Final Assessment_Stack_Expression_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int getInfix(string s) {
+    stack<int>st;
+    for(int i=0;i<s.size();i++) {
+        if(isdigit(s[i])) {
+            st.push(s[i]-'0');
+        }
+        
+        else {
+            int a1=st.top();
+            st.pop();
+            int a2 = st.top();
+            st.pop();
+            switch(s[i]) {
+                case '+':
+                st.push(a2+a1);
+                break;
+                case '-':
+                st.push(a2-a1);
+                break;
+                case '*':
+                st.push(a2*a1);
+                break;
+                case '/':
+                st.push(a2/a1);
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main() {
+    int size;
+    cin >>size;
+    int size1 =size;
+    vector<string> v;
+    while (size--) {
+        string s;
+        cin>>s;
+        v.push_back(s);
+    }
+    int i=0;
+    while(i<size1) {
+        cout << getInfix(v[i]) << endl;
+        i++;
+    }
+    return 0;
+}
